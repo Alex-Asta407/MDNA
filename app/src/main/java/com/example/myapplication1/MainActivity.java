8c1a99f0760c86c6ed47fb98045e70d7de1e000b
Oleksandr Konstantinov <akonstantinov582@gmail.com>
2023-11-11 17:35:27 +0200

    Updated files

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,11 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        TextView text = findViewById(R.id.textView);
+        Button button1 = findViewById(R.id.button1);
+        Button buttonChangeColor = findViewById(R.id.buttonChangeColor);
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hello My Friend", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        buttonChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setTextColor(Color.BLACK);
             }
         });
     }
